53205a7304a5db00df89746759d9458c959b2f7f
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-14 17:28:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       #your key/value pairs here!
       patriarch: {},
       matriarch: {},
-      heroine: {},
+      hero: {},
       hero_friends: []
       },
     :capulet => {
